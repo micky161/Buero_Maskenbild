ecc84686769ac015d845a19141041a3a12b2d254
Unknown <micky@systemli.org>
2018-09-14 23:16:43 +0200

    esclipse-workspace unziped

---
+++ eclipse-workspace/buero_maskenbild/buero_maskenbild/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for buero_maskenbild project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.0/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "buero_maskenbild.settings")
+
+application = get_wsgi_application()
